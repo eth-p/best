301a6c841bab72ff87ae35966e89142552ce9068
Ethan P <eth-p+git@hidden.email>
2021-03-30 16:49:01 -0700

    Add tests for Best

--- test.sh
+++ test.sh
@@ -9,19 +9,22 @@ HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${HERE}/lib/opt.sh"
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Test Environment:
-# ----------------------------------------------------------------------------------------------------------------------
-export TEST_ENV_LIB="${HERE}/lib"
-export TEST_ENV_PATH="${PATH}"
-export TEST_ENV_HOME="${HERE}/tests/data"
-
-# ----------------------------------------------------------------------------------------------------------------------
 # Test Files:
 # ----------------------------------------------------------------------------------------------------------------------
 export TEST_DIR="${HERE}/tests/${TEST_DIR:-test}"
 export TEST_PWD="${HERE}/tests/${TEST_PWD:-data}"
 export TEST_SHIM_PATH="${HERE}/tests/${TEST_SHIM_PATH:-shim}"
-export SNAPSHOT_DIR="${HERE}/tests/${TEST_SHIM_PATH:-test-snapshots}"
+export SNAPSHOT_DIR="${HERE}/tests/${SNAPSHOT_DIR:-test-snapshots}"
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Test Environment:
+# ----------------------------------------------------------------------------------------------------------------------
+export TEST_ENV_LIB="${HERE}/lib"
+export TEST_ENV_TEST_HELPERS="${HERE}/tests/helpers"
+export TEST_ENV_PATH="${PATH}"
+export TEST_ENV_HOME="${HERE}/tests/data"
+export TEST_ENV_BEST_EXECUTABLE="${HERE}/bin/best.sh"
+export TEST_ENV_TEST_DIR="${TEST_PWD}/meta-tests"
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Options:
